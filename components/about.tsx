625d0a5fd6a2b06c6cc95375dc4604e94fc76986
Tushar Shrivastav <74534919+Tuhsarshrivastav@users.noreply.github.com>
2024-12-08 15:52:30 +0530

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -19,27 +19,22 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-      After graduating with a degree in{" "}
-        <span className="font-medium">commerce</span>, I made the decision to pursue my passion for programming. I embarked on a journey of learning{" "}
-        <span className="font-medium">full-stack web development</span>. through online courses{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, React-Native, Vue.js Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with Angular  TypeScript and Prisma. I am always looking to
-        learn new technologies.
-        {/* I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer. */}
-      </p>
-      <p>
-        <span className="italic">When I'm not coding</span>, I enjoy going to the gym and powerlifting. Additionally, 
-    I have a strong interest in{" "}
-        <span className="font-medium">learning new things</span>.{" "}
-      </p>
+  <span className="font-medium">In college</span>, I embarked on a journey of learning{" "}
+  <span className="font-medium">full-stack web development</span> through online courses.{" "}
+  <span className="italic">My favorite part of programming</span> is the
+  problem-solving aspect. I <span className="underline">love</span> the
+  feeling of finally figuring out a solution to a problem. My core stack
+  is{" "}
+  <span className="font-medium">
+    React, React Native, Vue.js, Next.js, Node.js, and MongoDB
+  </span>. I am also familiar with DevOps and am always looking to
+  learn new technologies.
+</p>
+<p>
+  <span className="italic">When I'm not coding,</span> I enjoy going to the gym and powerlifting. Additionally, 
+  I have a strong interest in{" "}
+  <span className="font-medium">learning new things</span>.
+</p>
     </motion.section>
   );
 }
